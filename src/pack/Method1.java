1064e4824cab48d4a6ad6af7615606f4ef8274b1
sowjanya1104 <sowjanya1104@gmail.com>
2024-04-19 15:24:54 -0700

    commit changed

--- src/pack/Method1.java
+++ src/pack/Method1.java
@@ -1,6 +1,7 @@
 package pack;
 
 public class Method1 {
+	private static final String message = null;
 	public void message()
 	{
 	 System.out.println("hello");
@@ -10,6 +11,11 @@ public class Method1 {
 	{
 		System.out.println("my changes");
 		System.out.println("hello iam here");
+		
+	}
+	 public String hello()
+	{
+		return message;
 	}
 
 }
